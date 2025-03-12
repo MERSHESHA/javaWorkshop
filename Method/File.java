c8b9255a2aaef5135de7ef2aa9c9de017c5aa8c1
MERSHESHA <mershesha.m777@gmail.com>
2025-03-12 14:24:07 +0530

    program finished

---
+++ Method/File.java
@@ -0,0 +1,13 @@
+public class File{
+    public void add(int num1,int num2){
+        int num=num1+num2;
+        System.out.println(num);
+
+    }
+    public static void main(String args[]){
+        File obj=new File();
+        obj.add(50,20);
+
+
+    }
+}
