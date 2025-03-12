cb43c4a6b862469071167a4141353e21c859cfb2
MERSHESHA <mershesha.m777@gmail.com>
2025-03-12 13:54:46 +0530

    First program is finished

--- Variable/Demo.java
+++ Variable/Demo.java
@@ -1,11 +1,11 @@
-public class File{
+public class Demo{
     public void add(int num1,int num2){
         int num=num1+num2;
         System.out.println(num);
 
     }
     public static void main(String args[]){
-        File obj=new File();
+        Demo obj=new Demo();
         obj.add(10,20);
 
 
