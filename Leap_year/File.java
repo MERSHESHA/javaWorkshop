95247c5b53ba93ef31b5b69fdff2bad1737b2923
MERSHESHA <mershesha.m777@gmail.com>
2025-03-13 14:22:19 +0530

    program is completed

---
+++ Leap_year/File.java
@@ -0,0 +1,15 @@
+public class File{
+    public static void main(String args[]){
+        
+
+          int i=2020;
+       
+            if(i%4==0){
+            System.out.println("Leap year");
+            }
+            else{
+                 System.out.println(" not a Leap year");
+            }
+        }
+    }
+
