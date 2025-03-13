683de2e81a05adcbcd5bc5d14662484036763432
MERSHESHA <mershesha.m777@gmail.com>
2025-03-13 15:17:57 +0530

    the program is completed

---
+++ Dynamic_addition/File.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class File{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter a number1");
+        int number1=input.nextInt();
+        System.out.println("enter a number2");
+         int number2=input.nextInt();
+         int num=number1+number2;
+         System.out.println("total:"+num);
+    }
+}
