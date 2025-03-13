a9b52c83593bb3784fefb7d064fb363a2db181ff
MERSHESHA <mershesha.m777@gmail.com>
2025-03-13 11:42:59 +0530

    I finished

--- Positive_negative/File.java
+++ Positive_negative/File.java
@@ -1,8 +1,11 @@
 public class File{
     public static void main(String args[]){
-        int num =5;
-    if(num>=0){
+        int num =0;
+    if(num>0){
         System.out.println("positive");
+    }
+    else if(num==0){
+        System.out.println("neutral");
     }
     else{
          System.out.println("negative");
