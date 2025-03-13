96aa3f3975b58205233dc273eeca5af18a3d1226
MERSHESHA <mershesha.m777@gmail.com>
2025-03-13 11:27:31 +0530

    I finished

---
+++ Gender/File.java
@@ -0,0 +1,12 @@
+public class File{
+    public static void main(String args[]){
+        String gender="f";
+        if(gender=="m"){
+            System.out.println("the person is male");
+
+        }
+        else{
+            System.out.println("the person is female");
+        }
+    }
+}
