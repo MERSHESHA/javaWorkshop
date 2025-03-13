83cd91dc6723846bee4f85fe035664b391d1edc0
MERSHESHA <mershesha.m777@gmail.com>
2025-03-13 11:06:58 +0530

    I finished

---
+++ Major_minor_finder/File.java
@@ -0,0 +1,12 @@
+public class File{
+    public static void main(String args[]){
+        int age=20;
+        if(age>=18){
+            System.out.println("the persois major");
+
+        }
+        else{
+            System.out.println("the person is minor");
+        }
+    }
+}
