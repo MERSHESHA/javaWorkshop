2f22e136b98e3dfc74d05d35a3e2eee3a7c9f2e3
MERSHESHA <mershesha.m777@gmail.com>
2025-03-13 15:08:58 +0530

    program is completed

---
+++ Nextint/File.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class File{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter your age");
+        int age=input.nextInt();
+        
+    }
+}
