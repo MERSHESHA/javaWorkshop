99949fdac02cb3d7c1e25e8dd96bde6235017f51
MERSHESHA <mershesha.m777@gmail.com>
2025-03-13 10:56:55 +0530

    I finished

---
+++ If_else/File.java
@@ -0,0 +1,11 @@
+public class File{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("the statement is true");
+
+        }
+        else{
+            System.out.println("the statement is false");
+        }
+    }
+}
