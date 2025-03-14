ac21893ffa6293dff3fff4c69ce13bff467e0964
MERSHESHA <mershesha.m777@gmail.com>
2025-03-14 10:16:23 +0530

    program executed

---
+++ Table/File.java
@@ -0,0 +1,12 @@
+public class File{
+    public static void main(String args []){
+        int i=7;
+        int x=1;
+        while(i<=70){
+            System.out.println(x+ "x*7=" +i);
+               x=x+1;
+               i=i+7;
+            }
+             
+    }
+}
