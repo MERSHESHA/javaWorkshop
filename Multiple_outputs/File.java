c33826998d421ace5f3458301048ac233163c677
MERSHESHA <mershesha.m777@gmail.com>
2025-03-14 14:44:56 +0530

    program is completed

---
+++ Multiple_outputs/File.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class File{
+        public static void main(String args[]){
+          Scanner input=new Scanner(System.in);
+          System.out.println("Enter the n values");
+          int n =input.nextInt();
+          int a[]=new int[n];
+          int i=0;
+          while(i<n){
+            System.out.println("Enter the value");
+            a[i]=input.nextInt();
+            i=i+1;
+
+          }
+          while(i<n){
+            System.out.println("Entered the value:"+a[i]);
+            i=i+1;
+          }
+        }
+}
