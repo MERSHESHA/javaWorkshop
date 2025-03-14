d7be6aacccf67d68bfe04977cdd4503d54b7d522
MERSHESHA <mershesha.m777@gmail.com>
2025-03-14 10:02:32 +0530

    program executed

---
+++ While/File.java
@@ -0,0 +1,9 @@
+public class File{
+    public static void main(String args []){
+        int i=92;
+        while(i<=98){
+            i=i+1;
+            System.out.println(i);
+        }
+    }
+}
