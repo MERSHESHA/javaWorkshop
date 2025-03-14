877b334b7fe4eebec4699391e950a434d3abad64
MERSHESHA <mershesha.m777@gmail.com>
2025-03-14 11:50:10 +0530

    program is completed

---
+++ Split/File.java
@@ -0,0 +1,7 @@
+public class File{
+    public static void main(String args[]){
+        String name="I ,am,mershesha";
+        String names[]=name.split(",");
+        System.out.println(names[0]);
+    }
+}
