6cd8ecbf66d5d3e7c1281e4a71cf7ddba2b2ef6a
MERSHESHA <mershesha.m777@gmail.com>
2025-03-14 12:17:11 +0530

    program

---
+++ Method_overload/File.java
@@ -0,0 +1,16 @@
+public class File{
+    public void sub(int num1,int num2){
+            int num=num1-num2;
+            System.out.println(num);
+        }
+        public void sub(int num1,int num2,int num3){
+            int num=num1-num2-num3;
+            System.out.println(num);
+        }
+    public static void main(String args[]){
+       File obj=new File();
+       obj.sub(40,30);
+       obj.sub(40,60); 
+       
+    }
+}
