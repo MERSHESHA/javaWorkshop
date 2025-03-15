491feb2084cd3ecb207bcf42042917befa6551c3
MERSHESHA <mershesha.m777@gmail.com>
2025-03-15 11:29:56 +0530

    program completed

---
+++ Salary_generator/Fil.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Fil{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter your profession");
+        String name=input.nextLine();
+        System.out.println("salary");
+        double salary=input.nextInt();
+           
+           double Salarys=0;
+           
+        
+        if(name.equals("manager")){               
+          
+            salary=salary+salary*0.2;
+          
+            System.out.println(salary);
+        }
+       else if(name.equals("developer")){
+           
+            salary=salary+salary*0.1;
+          
+            System.out.println(salary);
+        }
+        else{
+            System.out.println("invalid profession");
+        }
+        
+    }
+}
